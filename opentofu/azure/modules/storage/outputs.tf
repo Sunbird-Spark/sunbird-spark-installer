47bc6beeb45c58198751ab976d7a0d6389f19bbe
Shashank <shashankp9353@gmail.com>
2026-02-25 12:52:57 +0530

    removed the commented lines realted to dial service

--- opentofu/azure/modules/storage/outputs.tf
+++ opentofu/azure/modules/storage/outputs.tf
@@ -3,7 +3,7 @@ output "azurerm_storage_account_name" {
 }
 
 output "azurerm_storage_account_key" {
-  value = azurerm_storage_account.storage_account.primary_access_key
+  value     = azurerm_storage_account.storage_account.primary_access_key
   sensitive = true
 }
 
@@ -14,11 +14,6 @@ output "azurerm_storage_container_private" {
 output "azurerm_storage_container_public" {
   value = azurerm_storage_container.storage_container_public.name
 }
-
-# DIAL container output commented - now managed by addons/dial/opentofu
-# output "azurerm_dial_state_container_public" {
-#   value = azurerm_storage_container.dial_state_container_public.name
-# }
 
 output "azurerm_velero_container_name" {
   value = azurerm_storage_container.velero_storage_container_private.name
