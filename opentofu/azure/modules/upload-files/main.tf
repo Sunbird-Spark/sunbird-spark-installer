add12b14719c02422b5ca1b5c6ae4074b53657d2
divyagovindaiah <110388603+divyagovindaiah@users.noreply.github.com>
2026-05-14 13:34:12 +0530

    Update main.tf

--- opentofu/azure/modules/upload-files/main.tf
+++ opentofu/azure/modules/upload-files/main.tf
@@ -230,5 +230,5 @@ resource "null_resource" "upload_rc_schemas_to_public_blob" {
   provisioner "local-exec" {
     command = "az storage blob upload-batch --account-name ${var.storage_account_name} --destination ${var.storage_container_public}/schemas --source ${path.module}/sunbird-rc/schemas --auth-mode login"
   }
-  depends_on = [local_file.output_files, null_resource.copy_from_sunbird_container]
+  depends_on = [local_file.output_files]
 }
